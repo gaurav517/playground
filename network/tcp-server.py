e7eebae1e524f9e5e08b4452b814a446e22a517a
Kumar Gaurav <gaurav@kgaurav.com>
2026-05-23 23:12:05 +1000

    chore: implement basic TCP server functionality

--- network/tcp-server.py
+++ network/tcp-server.py
@@ -8,3 +8,29 @@ serv_sock = socket.socket(
 
 print(type(serv_sock))   # <class 'socket.socket'>
 print(serv_sock.fileno())
+
+# bind to network interface (''/'0.0.0.0' means all interfaces) and port
+serv_sock.bind('0.0.0.0', 6543)
+
+# listen
+backlog = 10 # max number of established connections waiting for acceptance
+serv_sock.listen(backlog)
+
+print('server: ', serv_sock.getsockname())
+
+while True:
+    client_sock, client_addr = serv_sock.accept()
+    print('New connection from', client_addr)
+
+    print('printing peer:')
+    client_sock.getpeername()
+
+    chunks = []
+    while True:
+        data = client_sock.recv(2048)
+        if not data:
+            break
+        chunks.append(data)
+    client_sock.sendall(b''.join(chunks))
+    client_sock.close()
+
