d3ac336777e6c1b80268dd4ae8cf5217e88a4146
Kumar Gaurav <gaurav@kgaurav.com>
2026-05-23 21:43:37 +1000

    chore: initial commit

---
+++ network/tcp-server.py
@@ -0,0 +1,9 @@
+import socket
+
+serv_sock = socket.socket(
+    socket.AF_INET,      # set protocol family to 'Internet' (INET)
+    socket.SOCK_STREAM,  # set socket type to 'stream' (i.e. TCP)
+    proto=0              # set the default protocol (for TCP it's IP)
+)
+
+print(type(serv_sock))   # <class 'socket.socket'>
